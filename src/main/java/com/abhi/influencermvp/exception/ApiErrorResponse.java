a6dcc44cb2eefeabe742a59c2bf05b77316765d3
Abhisek542 <boseabhisek76@gmail.com>
2026-01-03 10:47:48 +0530

    Store Changes

--- src/main/java/com/abhi/influencermvp/exception/ApiErrorResponse.java
+++ src/main/java/com/abhi/influencermvp/exception/ApiErrorResponse.java
@@ -1,6 +1,6 @@
 package com.abhi.influencermvp.exception;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 
 @Data
--- src/main/java/com/abhi/influencermvp/repository/BranDashboardRepo.java
+++ src/main/java/com/abhi/influencermvp/repository/BranDashboardRepo.java
@@ -1,6 +1,6 @@
 package com.abhi.influencermvp.repository;
 
-import com.abhi.influencermvp.dto.BrandDashboardDto;
+
 import com.abhi.influencermvp.entity.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
