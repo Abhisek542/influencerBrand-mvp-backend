5aa321585607fa78047bd3c3479d2b5eee89f638
Abhisek542 <boseabhisek76@gmail.com>
2026-04-08 19:32:46 +0530

    Commit for changes in SecurityConfig for cors issue of frontend

--- src/main/java/com/abhi/influencermvp/config/SecurityConfig.java
+++ src/main/java/com/abhi/influencermvp/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOriginPatterns(List.of("*")); // ✅ allow all (dev + prod)
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
